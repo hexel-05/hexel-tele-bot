fe1ef93b6d29fd5209ce7aa1c93a985b0196b888
hexel-05 <144644352+hexel-05@users.noreply.github.com>
2025-02-21 13:58:29 +0700

    Add delay

--- src/command/start.js
+++ src/command/start.js
@@ -1,17 +1,21 @@
 const { getMenuKeyboard } = require('../keyboard/keyboard');
-const log = require('../utils/logger');
+const log = require('../middlewares/logger');
+const sleep = require('../utils/sleep');
 
 module.exports = (bot) => {
-    bot.command('start', (ctx) => {
-        ctx.reply('Test' , getMenuKeyboard());
+    bot.command('start', async (ctx) => {
+        await sleep(500);
+        await ctx.reply('Test' , getMenuKeyboard());
         log.info(`@${ctx.from.username} using command /start`);
     });
 
-    bot.on('callback_query' , (ctx) => {
+    bot.on('callback_query' , async (ctx) => {
         const callbackData = ctx.callbackQuery.data;
 
         if (callbackData === 'info') {
-            ctx.reply('Informasi bla bla');
+            await sleep(1000);
+            await ctx.deleteMessage();
+            await ctx.reply('Informasi bla bla');
         } else {
             console.log('Error callback data not found');
         }
