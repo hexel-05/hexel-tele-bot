3e3028683d14dcdac2d062e7672d70f63e32b5c2
hexel-05 <144644352+hexel-05@users.noreply.github.com>
2025-02-21 09:35:58 +0700

    Fix log

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,24 +1,22 @@
 const winston = require('winston');
+const {format} = require('date-fns')
+
+const formatLogger = winston.format.printf(({level , message , timestamp}) => {
+  const formattedTimestamp = format(new Date(timestamp), 'dd-MM-yyyy HH:mm:ss');  
+  return `${formattedTimestamp} - ${level} ${message}`;
+});
 
 const logger = winston.createLogger({
   level: 'info',
   format: winston.format.combine(
-    winston.format.timestamp({
-      format: 'YYYY-MM-DD HH:mm:ss',
-    }),
-    winston.format.simple(),
+    winston.format.timestamp(),
+    formatLogger
   ),
   transports: [
-    new winston.transports.Console({
-      format: winston.format.combine(
-        winston.format((info) => {
-          delete info.timestamp;
-          return info;
-        })(),
-        winston.format.simple(),
-      ),
-    }),
-    new winston.transports.File({ filename: 'bot.log' }),
+    new winston.transports.Console(),
+    new winston.transports.File({
+      filename: 'bot.log'    
+    })
   ],
 });
 
