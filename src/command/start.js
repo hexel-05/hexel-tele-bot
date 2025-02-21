f843101168c62e50617907f532ee8273eb0087e8
hexel-05 <144644352+hexel-05@users.noreply.github.com>
2025-02-21 09:34:59 +0700

    Added new inline keyboard

--- src/command/start.js
+++ src/command/start.js
@@ -1,7 +1,19 @@
 const { getMenuKeyboard } = require('../keyboard/keyboard');
+const log = require('../utils/logger');
 
 module.exports = (bot) => {
     bot.command('start', (ctx) => {
-        ctx.reply('Test');
+        ctx.reply('Test' , getMenuKeyboard());
+        log.info(`@${ctx.from.username} using command /start`);
     });
+
+    bot.on('callback_query' , (ctx) => {
+        const callbackData = ctx.callbackQuery.data;
+
+        if (callbackData === 'info') {
+            ctx.reply('Informasi bla bla');
+        } else {
+            console.log('Error callback data not found');
+        }
+    })
 }
